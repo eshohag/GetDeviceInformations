edbb1810a61ea02bb152f18880d6b348a1f399ab
MD. Shohag Mia <mdshohagmia14@outlook.com>
2018-09-06 00:33:02 +0600

    fdg

--- GetDeviceInformations/Program.cs
+++ GetDeviceInformations/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,26 @@ namespace GetDeviceInformations
     {
         static void Main(string[] args)
         {
+            var check= System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+
+            if (check)
+            {
+                Console.WriteLine(GetLocalIPAddress());
+            }
+           
+            Console.ReadKey();
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
